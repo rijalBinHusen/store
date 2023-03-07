4355bb05f03b6648f2d4316c7a4e98ad8d439be6
rijalBinHusen <jeryzal333@gmail.com>
2023-03-07 16:10:58 +0700

    add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue()],
+    integrations: [vue(), tailwind({
+        config: { path: './tailwind.config.js'}
+    })],
     vite: {
         resolve: {
             alias: {
